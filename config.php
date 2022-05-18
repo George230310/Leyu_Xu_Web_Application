780ceff0033a2bd1561f3b7c453e39509cde2f9c
Leyu Xu <leyuxu@usc.edu>
2022-05-18 00:07:38 -0700

    fix conflict

--- config.php
+++ config.php
@@ -2,13 +2,8 @@
 	session_start();
 	date_default_timezone_set('America/Los_Angeles');
 	// Define constants - constants cannot be changed after they have been declared.
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$db_host = $url["host"];
-	$db_user = $url["user"];
-	$db_pass = $url["pass"];
-	$db_name = substr($url["path"],1);
-	define("DB_HOST", $url["host"]);
-	define("DB_USER", $url["user"]);
-	define("DB_PASS", $url["pass"]);
-	define("DB_NAME", substr($url["path"],1));
+	define("DB_HOST", "host");
+	define("DB_USER", "user");
+	define("DB_PASS", "password");
+	define("DB_NAME", "name");
 ?>
